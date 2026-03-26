36374d4009df3f5d937125b8a4df0823bd554632
unknown <hritikawadhai823@gmail.com>
2026-03-26 15:47:47 +0530

    Student registration form without API'S

--- Backened/test-export.js
+++
@@ -1,3 +0,0 @@
-const db = require('./db');
-console.log("Exported config:", db.config);
-console.log("Full export:", db);
--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./api/students");
-
-const PORT = 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://10.206.86.78:${PORT}`);
-});
